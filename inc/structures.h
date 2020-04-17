e751a12c9315b526919007790cf41dcf72be06ff
ronald112 <ronald11211@gmail.com>
2020-04-17 21:16:13 +0300

    Update structures.h

--- inc/structures.h
+++ inc/structures.h
@@ -3,8 +3,18 @@
 #include "defines.h"
 
 typedef struct {
-    char *i_hist[MAX_HIST_BUF + 1];
-    char i_str[MAX_LNG_LINE + 1];
-    char *parsed_args[MAX_HIST_BUF + 1];
+    bool pipe;
+    char *parsed_args[MAX_HIST_BUF];
+} t_command;
+
+typedef struct {
+    char i_str[MAX_LNG_LINE];
+    t_history *next;
+} t_history;
+
+typedef struct {
+    char *i_hist[MAX_HIST_BUF];
+    char i_str[MAX_LNG_LINE];
+    t_command *parsed_args;
     int nm;
 } t_ush;
